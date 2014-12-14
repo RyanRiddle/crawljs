2fd028ccdf4435c77337eabc07f8938c601cf7d1
Ryan <rcri222@gmail.com>
2014-12-13 22:59:00 -0500

    added the robots package to parse robots.txt and avoid going to disallowed pages.  added a url manipulator class to get the robots.txt path and path without the hostname.

--- crawl.js
+++ crawl.js
@@ -1,50 +1,107 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var urlManipulator = require('url');
+var robots = require('robots'), robotsParser = new robots.RobotsParser();
+
+var UrlManipulator = function()
+{
+	this.url = require('url');
+};
+
+UrlManipulator.prototype.getRobotsTxtPath = function(url)
+{
+	parsedUrl = this.url.parse(url);
+	var robotsTxtPath = parsedUrl.protocol + (parsedUrl.slashes ? "//" : "") + parsedUrl.host + "/robots.txt";
+	return robotsTxtPath;
+};
+
+UrlManipulator.prototype.resolve = function(url, link)
+{
+	return this.url.resolve(url, link);
+};
+
+UrlManipulator.prototype.getPath = function(url)
+{
+	return this.url.parse(url).path;
+};
 
 var Crawler = function(seedValues)
 {
 	this.seedValues = seedValues || ["http://ryanriddle.info"];
 	this.pagesQueue = this.seedValues.slice(0).reverse();	//slice(0).reverse() copies the array.
 	this.url = this.pagesQueue.shift();
-	this.pagesVisited = [];
-	this.pagesFailed = [];
+	this.visitedPages = [];
+	this.failedPages = [];
+	this.disallowedPages = [];
+	this.urlManipulator = new UrlManipulator();
+};
+
+Crawler.prototype.getRobotsTxtPath = function(url)
+{
+	return this.urlManipulator.getRobotsTxtPath(url);
 };
 
 Crawler.prototype.requestAndHandle = function()
 {
-	if (typeof this.url !== "string")
+	var url = this.url;
+
+	if (typeof url !== "string")
 		return;
 	
-	if (this.pagesVisited.indexOf(this.url) < 0)
+	if (this.visitedPages.indexOf(url) < 0)
 	{
 		var that = this;
-		request(this.url, function(error, response, body)
-		{	
-			if (!error && response.statusCode === 200)
+		robotsParser.setUrl(this.getRobotsTxtPath(url), function(parser, success)
+		{
+			if (success)
 			{
-				console.log(that.url);
-				$ = cheerio.load(body);
-				var aTags = $('a');
-				for (var i = 0; i < aTags.length; i++)
+				parser.canFetch("*", that.urlManipulator.getPath(url), function(access)
 				{
-					var tag = aTags[i];
-					var link = $(tag).attr('href');
-					var newURL = link && urlManipulator.resolve(that.url, link);
-					if (newURL) that.pagesQueue.push(newURL);
-				}
-				that.pagesVisited.push(that.url);
+					if (access)
+					{
+						request(url, function(error, response, body)
+						{	
+							if (!error && response.statusCode === 200)
+							{
+								console.log(url);
+								$ = cheerio.load(body);
+								var aTags = $('a');
+								for (var i = 0; i < aTags.length; i++)
+								{
+									var tag = aTags[i];
+									var link = $(tag).attr('href');
+									var newURL = link && that.urlManipulator.resolve(url, link);
+									if (newURL) that.pagesQueue.push(newURL);
+								}
+								that.visitedPages.push(url);
+							}
+							else
+							{
+								console.log("failed to fetch page");
+								that.failedPages.push(url);
+							}
+
+							that.url = that.pagesQueue.shift();
+							that.requestAndHandle();
+						});
+					}
+					else
+					{
+						console.log("disallowed in robots.txt");
+						that.disallowedPages.push(url);
+						that.url = that.pagesQueue.shift();
+						that.requestAndHandle();
+					}
+				});
 			}
 			else
 			{
-				console.log("failed to fetch page");
-				that.pagesFailed.push(that.url);
+				console.log("robots setUrl failed");
+				that.failedPages.push(url);
+				that.url = that.pagesQueue.shift();
+				that.requestAndHandle();
 			}
-
-			that.url = that.pagesQueue.shift();
-			that.requestAndHandle();
 		});
-	}
+	}			
 	else
 	{
 		console.log("already visited this page");
@@ -53,5 +110,5 @@ Crawler.prototype.requestAndHandle = function()
 	}
 };
 
-var crawler = new Crawler(["http://stadiumfanatic.com"]);
+var crawler = new Crawler(["http://reddit.com"]);
 crawler.requestAndHandle();
